e79ac3bd95917f1d7a6c8d64b39d086e2b29fd05
DenisTheProgrammer <denisnic801@gmail.com>
2024-11-25 18:20:31 +0000

    made it so data is fetched from a url instead of a file

--- src/components/screens/ModuleCrudlerScreen.js
+++ src/components/screens/ModuleCrudlerScreen.js
@@ -1,18 +1,28 @@
-import { StyleSheet, LogBox } from 'react-native';
+import { StyleSheet, LogBox, Text } from 'react-native';
 import Screen from "../layout/Screen.js";
 
-import initialModules from '../../data/modules.js';
+import API from "../API/API.js";
 import ModuleList from "../entity/modules/ModuleList.js";
 import { ButtonTray, Button } from '../UI/Button.js';
 import Icons from '../UI/Icons.js';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const ModuleCrudlerScreen = ({navigation}) => {
   // Initialisations -------------------
   LogBox.ignoreLogs([`Non-serializable values were found in the navigation state`]);
+  const modulesEndPoint = "https://softwarehub.uk/unibase/api/modules";
 
   // State -----------------------------
-  const [modules, setModules] = useState(initialModules);
+  const [modules, setModules] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadModules = async(endpoint) => {
+    const response = await API.get(endpoint);
+    setIsLoading(false);
+    if(response.isSuccess) setModules(response.result);
+  };
+
+  useEffect(() => {loadModules(modulesEndPoint)}, []);
 
   // Handlers --------------------------
   const handleAdd = (module) => setModules([...modules, module]);
@@ -52,6 +62,9 @@ export const ModuleCrudlerScreen = ({navigation}) => {
       <ButtonTray>
         <Button label = "Add" icon = {<Icons.Add/>} onClick = {goToAddScreen}/>
       </ButtonTray>
+      {
+        isLoading && <Text>Loading records ...</Text>
+      }
       <ModuleList modules = {modules} onSelect={goToViewScreen}/>
     </Screen>
   );
